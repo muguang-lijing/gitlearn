ee7b4bc68c250a982c65e2110f60e1a283018632
lijing <lijing@yeezon.com>
2016-08-05 16:16:30 +0800

    hehe

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 "use strict"
 let js = require('fs');
-
+console.time("read File");
 fs.readFile(process.argv[2],()=>{
 	console.log("name");
+	console.log("jiu");
+	console.timeEnd("read File");
 });
